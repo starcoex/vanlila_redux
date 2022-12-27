90ccd81b31e350f307a83e276bf528ab8b3ac698
starcoex <69073390+starcoex@users.noreply.github.com>
2022-12-27 12:47:10 +0900

    Vanilla setting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { legacy_createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+
+let counter = 0;
+number.innerText = counter;
+
+const update = () => {
+	number.innerText = counter;
+};
+
+const handleClick = () => {
+	counter = counter + 1;
+	update();
+};
+const handleMinus = () => {
+	counter = counter - 1;
+	update();
+};
+
+add.addEventListener('click', handleClick);
+minus.addEventListener('click', handleMinus);
